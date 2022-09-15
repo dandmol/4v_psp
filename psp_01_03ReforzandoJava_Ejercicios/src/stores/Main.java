e099754e08183cc78b09f7bc5a8064aa596c312d
dandmol <dandmol@gmail.com>
2022-09-15 14:09:48 +0200

    clase anónima

--- psp_01_03ReforzandoJava_Ejercicios/src/stores/Main.java
+++ psp_01_03ReforzandoJava_Ejercicios/src/stores/Main.java
@@ -14,9 +14,30 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String euro = " \u20AC"; //símbolo del euro en unicode
 		LiquorStore puertoDeIndias = new LiquorStore(8.95,20);
 		
+		System.out.println(euro);
+		
+		puertoDeIndias.welcome();
 		puertoDeIndias.payDrinks(10);
-		System.out.printf("Wellcome to anonumous store!, Our drink price is %.2f€",puertoDeIndias.getCash());
+		System.out.printf("The price is %.2f€",puertoDeIndias.getCash());
+		
+		// Instancio clase anónima
+		Store campoViejo = new Store(8.95) {
+
+			@Override
+			void welcome() {
+				// TODO Auto-generated method stub
+				System.out.printf("Welcome to anonymous store!, Our drink price is %.2f €",this.getDrinkPrice());
+				
+			}
+		};
+		System.out.println();
+		System.out.println("*** Anonymous class ***");
+		campoViejo.welcome();
+		campoViejo.payDrinks(10);
+		System.out.printf("\nThe price is %.2f €",campoViejo.getCash());
+		
 	}
 }
