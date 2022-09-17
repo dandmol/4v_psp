ea177f090c65e6c91eaea20c7f2b2a8945823f47
dduran <dduran@192.168.0.21>
2022-09-17 17:31:24 +0200

    kkkk

--- kik/src/kik/HiWorld.java
+++ kik/src/kik/HiWorld.java
@@ -14,7 +14,7 @@ public class HiWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo!!!!");
+		System.out.println("Hola mundo!!");
 	}
 
 }
