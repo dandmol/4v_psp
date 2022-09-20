011c40cc2effa70b1daf06e7568856065db3e643
dduran <dduran@DESKTOP-NUBIB7Q>
2022-09-20 18:41:22 +0200

    UD2 ejercicios

--- psp_02_01_Colecciones/src/listasPR/Main.java
+++ psp_02_01_Colecciones/src/listasPR/Main.java
@@ -3,6 +3,9 @@
  */
 package listasPR;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * @author dduran
  *
@@ -14,7 +17,24 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<String> letras = new ArrayList<String>();
+		letras.add("aa");
+		letras.add("bb");
+		letras.add("cc");
+		letras.add("dd");
+		letras.add(2,"ee");
+		
+		for (String letra : letras) {
+			System.out.println(letra);
+		}
+		if (letras.contains("bb")) {
+			System.out.println("Lo contiene");
+		}
+		
+		Random x = new Random();
+		
+		System.out.println(x.nextInt(50)+1);
+		System.out.println((int)(Math.random()*10));
 	}
 
 }
