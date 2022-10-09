98d570e2a7ab3b4f6c574c3690e1d2ffbcd8f345
dandmol <33481216+dandmol@users.noreply.github.com>
2022-10-09 22:20:06 +0200

    Delete psp_02_01_Colecciones directory

--- psp_02_01_Colecciones/src/customException/BigRange.java
+++
@@ -1,8 +0,0 @@
-package customException;
-
-public class BigRange extends Exception {
-	public BigRange (int a, int b) {
-		super("BigRange: excepción absurda por que hay mucha diferencia entra " + a + " y " + b);
-	}
-
-}
